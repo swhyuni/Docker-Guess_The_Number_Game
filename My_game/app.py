c9c2f420fac99b2b5290151d1528cd4edd5f62dc
swhyuni <sriwahyuni1041997@gmail.com>
2023-08-08 10:03:23 +0700

    first commit

---
+++ My_game/app.py
@@ -0,0 +1,21 @@
+import pandas as pd 
+import numpy as np
+
+def number_guessing_game():
+    print('''
+    Selamat datang dipermainan tebak angka
+    Silakan tebak angka 0-9 sebenyak 3 kqli      
+    ''')
+
+    computer_number = np.random.randint(0,9,3)
+    user_number = []
+    for loop in range(3):
+        number = int(input(f'Masukan angka ke {loop+1}: '))
+        user_number.append(number)
+
+    result = list(user_number == computer_number)
+    output = pd.DataFrame([user_number, computer_number, result], index = ['Angka Anda', 'Angka Komputer', 'Hasil']).T
+    print(output)
+
+if __name__ == '__main__':
+    number_guessing_game()
